22242fec160e628dc79da382ec60c0ffce5206ce
gmushini <gmushini@host.docker.internal>
2022-07-28 22:44:27 +0530

    StacksLast

--- src/com/stacks/MAXMINDifference.java
+++ src/com/stacks/MAXMINDifference.java
@@ -1,18 +1,122 @@
 package com.stacks;
 
-import com.util.Util;
+import java.util.Stack;
 
 public class MAXMINDifference {
 
-	public static void main(String[] args) {
+	public int solve(int[] A) {
 
-		int[] A = new int[] { 5, 2, 1, 3, 6, 8, 1, 1, 8 };
+		long ans = 0;
+		int mod = (int) Math.pow(10, 9) + 7;
 
-		Util.printArray(new NearestSmallestLeft().nearestSmallLeft(A));
-		Util.printArray(new NearestSmallestRight().nearestSmallRight(A));
-		Util.printArray(new NearestLargestLeft().nearestLargeLeft(A));
-		Util.printArray(new NearestLargestRight().nearestLargeRight(A));
+		int[] nsl = nearestSmallLeft(A);
+		int[] nsr = nearestSmallRight(A);
+		int[] nll = nearestLargeLeft(A);
+		int[] nlr = nearestLargeRight(A);
 
+		long max = 0;
+		long min = 0;
+
+		for (int i = 0; i < A.length; i++) {
+			min = ((long) (i - nsl[i])) * (nsr[i] - i);
+			min *= A[i];
+
+			max = ((long) (i - nll[i])) * (nlr[i] - i);
+			max *= A[i];
+
+			ans += (max - min);
+
+		}
+
+		if (ans < 0) {
+			ans = (ans + mod) % mod;
+		}
+		ans %= mod;
+		return (int) ans;
+	}
+
+	public int[] nearestSmallLeft(int[] A) {
+		Stack<Integer> st = new Stack<>();
+		int[] res = new int[A.length];
+
+		for (int i = 0; i < A.length; i++) {
+
+			while (!st.isEmpty() && A[st.peek()] >= A[i])
+				st.pop();
+
+			if (st.isEmpty()) {
+				res[i] = -1;
+			} else
+				res[i] = st.peek();
+
+			st.push(i);
+
+		}
+
+		return res;
+	}
+
+	public int[] nearestSmallRight(int[] A) {
+		Stack<Integer> st = new Stack<>();
+		int[] res = new int[A.length];
+
+		for (int i = A.length - 1; i >= 0; i--) {
+
+			while (!st.isEmpty() && A[st.peek()] >= A[i])
+				st.pop();
+
+			if (st.isEmpty())
+				res[i] = A.length;
+			else
+				res[i] = st.peek();
+
+			st.push(i);
+
+		}
+
+		return res;
+	}
+
+	public int[] nearestLargeLeft(int[] A) {
+		Stack<Integer> st = new Stack<>();
+		int[] res = new int[A.length];
+
+		for (int i = 0; i < A.length; i++) {
+
+			while (!st.isEmpty() && A[st.peek()] <= A[i])
+				st.pop();
+
+			if (st.isEmpty())
+				res[i] = -1;
+			else
+				res[i] = st.peek();
+
+			st.push(i);
+
+		}
+
+		return res;
+	}
+
+	public int[] nearestLargeRight(int[] A) {
+		Stack<Integer> st = new Stack<>();
+		int[] res = new int[A.length];
+
+		for (int i = A.length - 1; i >= 0; i--) {
+
+			while (!st.isEmpty() && A[st.peek()] <= A[i])
+				st.pop();
+
+			if (st.isEmpty())
+				res[i] = A.length;
+			else
+				res[i] = st.peek();
+
+			st.push(i);
+
+		}
+
+		return res;
 	}
 
 }
