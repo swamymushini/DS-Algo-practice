b295d8b6f363935577f84cd49625c8f9501e3dc5
gopal <swamymushini@gmail.com>
2022-08-25 22:41:25 +0530

    Trees

--- src/com/trees/ValidateBST.java
+++ src/com/trees/ValidateBST.java
@@ -1,27 +1,69 @@
 package com.trees;
 
- //range approach
+//range approach
 public class ValidateBST {
-	
-	
-    public int isValidBST(TreeNode A) {
-        boolean valid = checkBST(A,Integer.MIN_VALUE,Integer.MAX_VALUE);
-        return valid?1:0;
-    }
 
-    public boolean checkBST(TreeNode A, int min , int max){
+	public static void main(String[] args) {
+		TreeNode root = TreeNode.buildTree(new Integer[][] { { 25 }, { 7, 36 }, { 10, 22, 30, 40 },
+				{ 5, 12, null, null, 28, 32, 38, 48 }, {}, {} });
+		System.out.println(new ValidateBST().isValidBST(root));
+		System.out.println(new ValidateBST().isBST(root).isBST);
+	}
 
-        if(A==null){
-            return true;
-        }
+	class TreeInfo {
 
-        if(A.val>=min&&A.val<=max){
+		int max;
+		int min;
+		TreeNode node;
+		boolean isBST;
 
-            return checkBST(A.left,min,A.val-1)&&
-                   checkBST(A.right,A.val+1,max);
-        }
+		public TreeInfo(int min, int max, TreeNode node, boolean isBST) {
+			super();
+			this.max = max;
+			this.min = min;
+			this.node = node;
+			this.isBST = isBST;
+		}
 
+	}
 
-        return false;
-    }
+	public int isValidBST(TreeNode A) {
+		boolean valid = checkBST(A, Integer.MIN_VALUE, Integer.MAX_VALUE);
+		return valid ? 1 : 0;
+	}
+
+	public boolean checkBST(TreeNode A, int min, int max) {
+
+		if (A == null) {
+			return true;
+		}
+
+		if (A.val >= min && A.val <= max) {
+
+			return checkBST(A.left, min, A.val - 1) && checkBST(A.right, A.val + 1, max);
+		}
+
+		return false;
+	}
+
+	public TreeInfo isBST(TreeNode A) {
+
+		if (A == null) {
+			return new TreeInfo(Integer.MAX_VALUE, Integer.MIN_VALUE, null, true);
+		}
+
+		TreeInfo l = isBST(A.left);
+		TreeInfo r = isBST(A.right);
+
+		Integer val = A.val;
+		
+		int mini = Math.min(val, l.min);
+		int maxi = Math.max(val, r.max);
+
+		if (val > l.max && val <= r.min && r.isBST && l.isBST) {
+			return new TreeInfo(mini, maxi, A, true);
+		}
+
+		return new TreeInfo(mini, maxi, A, false);
+	}
 }
