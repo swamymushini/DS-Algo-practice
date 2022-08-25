e8d75b9b93daa62b294600b8c2472a3e34b682db
gopal <swamymushini@gmail.com>
2022-08-25 23:04:12 +0530

    Trees

--- src/com/trees/LargestBSTSubtree.java
+++ src/com/trees/LargestBSTSubtree.java
@@ -1,8 +1,14 @@
 package com.trees;
 
 public class LargestBSTSubtree {
-	public int solve(TreeNode A) {
 
+	public static void main(String[] args) {
+		TreeNode root = TreeNode.buildTree(new Integer[][] { { 25 }, { 7, 36 }, { 10, 22, 30, 40 },
+				{ 5, 12, null, null, 28, 32, 38, 48 }, {}, {} });
+		System.out.println(new LargestBSTSubtree().solve(root));
+	}
+
+	public int solve(TreeNode A) {
 		return isBST(A).count + 1;
 	}
 
@@ -24,7 +30,7 @@ public class LargestBSTSubtree {
 	public TreeInfo isBST(TreeNode A) {
 
 		if (A == null)
-			return new TreeInfo(Integer.MAX_VALUE, Integer.MIN_VALUE, true, 0);
+			return new TreeInfo(Integer.MAX_VALUE, Integer.MIN_VALUE, true, -1);
 
 		TreeInfo l = isBST(A.left);
 		TreeInfo r = isBST(A.right);
@@ -32,7 +38,7 @@ public class LargestBSTSubtree {
 		int val = A.val;
 
 		int mini = Math.min(A.val, l.min);
-		int maxi = Math.min(A.val, r.max);
+		int maxi = Math.max(A.val, r.max);
 
 		if (val > l.max && val <= r.min && r.isBST && l.isBST) {
 			int count = r.count + l.count;
