5b9670721b876aacbf94c2663d9b18c3fe6a618f
gmushini <gmushini@RS-PF2DWQB5.rocketsoftware.com>
2022-09-11 18:12:05 +0530

    Trees

--- src/com/bitManipulations/Test.java
+++ src/com/bitManipulations/Test.java
@@ -3,6 +3,10 @@ package com.bitManipulations;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println(9 & -9);
+		String str = "Welcome";
+		String str2 = "Welcome";
+		
+		System.out.println(str==str2);
+		System.out.println(str.equals(str2));
 	}
 }
