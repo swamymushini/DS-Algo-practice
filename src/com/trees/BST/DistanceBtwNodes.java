b37eeabbcfab856f49d30a57fb5c72f190f5c541
gmushini <gmushini@RS-PF2DWQB5.rocketsoftware.com>
2022-10-03 17:09:59 +0530

    trees

--- src/com/trees/BST/DistanceBtwNodes.java
+++ src/com/trees/BST/DistanceBtwNodes.java
@@ -1,23 +1,58 @@
-package com.trees;
+package com.trees.BST;
+
+import com.trees.TreeNode;
 
 public class DistanceBtwNodes {
 
-	public int solve(TreeNode A, int B, int C) {
+	public static void main(String[] args) {
+		TreeNode root = TreeNode.buildTree(new Integer[][] { { 6 }, { 2, 15 }, { 1, 4, 10, 18 },
+				{ null, null, null, null, 9, 12, 17, 19 },
+				{ null, null, null, null, null, null, null, null, 8, null, null, null, null, null, null, null } });
+		System.out.println(new DistanceBtwNodes().solve(root, 1, 15));
+	}
+
+	int findHeight(TreeNode A, int B) {
 
 		if (A == null)
-			return 0;
+			return -1;
 
-		if (A.val == B || A.val == C) 
+		if (A.val < B)
+			return findHeight(A.right, B) + 1;
+		else if (A.val > B)
+			return findHeight(A.left, B) + 1;
+		else
 			return 0;
+	}
 
-		if (A.val > B && A.val > C) {
-			return solve(A.left, B, C);
-		} else if (A.val < B && A.val < C)
-			return solve(A.right, B, C);
-		else {
-			return solve(A.right, B, C) + solve(A.left, B, C) + 2;
+	public int solve(TreeNode A, int B, int C) {
+		TreeNode lca = findLCA(A, B, C);
+
+		if (lca.val == B) {
+			return findHeight(lca, C);
+		} else if (lca.val == C) {
+			return findHeight(lca, B);
 		}
-		return C;
+		return findHeight(lca, C) + findHeight(lca, B);
+	}
+
+	TreeNode findLCA(TreeNode A, int B, int C) {
+
+		if (A == null)
+			return null;
+
+		if (A.val == B || A.val == C)
+			return A;
+
+		TreeNode left = findLCA(A.left, B, C);
+		TreeNode right = findLCA(A.right, B, C);
+
+		if (left != null && right != null) {
+			return A;
+		} else if (left != null)
+			return left;
+		else
+			return right;
+
 	}
 
 }
